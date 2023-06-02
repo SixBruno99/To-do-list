790e5620b046f89cc5b1dffa3ce057c9b7249f4a
Bruno Henrique <brunohc2016@gmail.com>
2023-06-02 18:56:59 -0300

    mudanças no layout e bottomnavigationbar adicionado para ir para uma página diferente para exibir os itens favoritos

--- lib/widgets/show_list.dart
+++ lib/widgets/show_list.dart
@@ -8,7 +8,7 @@ import 'create_item.dart';
 import 'edit_item.dart';
 
 class ToDoListScreen extends StatelessWidget {
-  const ToDoListScreen({Key? key}) : super(key: key);
+  const ToDoListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,43 +17,59 @@ class ToDoListScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de To-Do'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
       ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                final dateController = TextEditingController();
-                final textController = TextEditingController();
+      body: Container(
+        color: const Color(0xFF181818),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+              child: ElevatedButton(
+                onPressed: () {
+                  final dateController = TextEditingController();
+                  final textController = TextEditingController();
 
-                showDialog(
-                  context: context,
-                  builder: (context) => CreateToDoItem(
-                    dateController: dateController,
-                    textController: textController,
-                    repository: toDoList,
+                  showDialog(
+                    context: context,
+                    builder: (context) => CreateToDoItem(
+                      dateController: dateController,
+                      textController: textController,
+                      repository: toDoList,
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
                   ),
-                );
-              },
-              child: Text('Criar Nova Anotação'),
+                  backgroundColor: Colors.green,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'Criar Nova Anotação',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: toDoList.toDoList.length,
-              itemBuilder: (context, index) {
-                final item = toDoList.toDoList[index];
-                return Dismissible(
+            Expanded(
+              child: ListView.builder(
+                itemCount: toDoList.toDoList.length,
+                itemBuilder: (context, index) {
+                  final item = toDoList.toDoList[index];
+                  return Dismissible(
                     key: Key(item.date + item.text),
                     onDismissed: (direction) {
                       toDoList.removeItem(item);
                     },
                     background: Container(
-                      color: Colors.blue,
+                      color: Colors.green,
                       alignment: Alignment.centerRight,
-                      padding: EdgeInsets.only(right: 16.0),
+                      padding: const EdgeInsets.only(right: 16.0),
                       child: Icon(
                         Icons.delete,
                         color: Colors.white,
@@ -77,15 +93,23 @@ class ToDoListScreen extends StatelessWidget {
                         );
                       },
                       child: ListTile(
-                        title: Text(item.date),
-                        subtitle: Text(item.text),
-                        trailing: Text("Deslize para deletar"),
+                        title: Text(item.date,
+                            style: TextStyle(
+                                color: Colors.white)), // Cor da fonte branca
+                        subtitle: Text(item.text,
+                            style: TextStyle(
+                                color: Colors.white)), // Cor da fonte branca
+                        trailing: Text("Deslize para deletar",
+                            style: TextStyle(
+                                color: Colors.white)), // Cor da fonte branca
                       ),
-                    ));
-              },
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
